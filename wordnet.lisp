3d5f65e8269bf6de466f34d34a9f8f245d26e473
dthole <dthole@ubuntu-serv.localdomain>
2009-12-03 15:03:34 -0600

    adding classes

--- wordnet.lisp
+++ wordnet.lisp
@@ -20,6 +20,13 @@
 
 (in-package :wordnet)
 
+;;;; Disable caching, mostly for debugging at this point ;;;;
+(setf clsql:*cache-table-queries-default* nil)
+
+;;;; Setup wordnet database global objects ;;;;
+(defvar *mssql* nil)
+(defvar *sqlite* nil)
+
 (defun connect-wordnet-mssql ()
   (setf *mssql* (clsql:connect '("wordnet" "wordnet" "wordnet") 
 			       :database-type :odbc
@@ -29,5 +36,43 @@
   (setf *sqlite* (clsql:connect '("/tmp/test.sqlite3") 
 				:database-type :sqlite3
 				:make-default nil)))
+
+;;;;; VIEW CLASS DEFINITIONS ;;;;;
+; Each of the below are class definitions that we want to utilize
+; when calling our methods
+;
+
+(clsql:def-view-class worddef () 
+  ((lemma :accessor lemma :type (string 80) :initarg :lemma)
+   (pos :accessor pos :type (string 3) :initarg :pos)
+   (definition :accessor definition :type (string 9000) :initarg :definition))
+  (:base-table word_defs))
+
+(clsql:def-view-class wordparse()
+  ((lemma :accessor lemma :type (string 80) :initarg :lemma)
+   (synsetid :accessor synsetid :type integer :initarg :synsetid)
+   (parse :accessor parse :type (string 9000) :initarg :parse))
+  (:base-table word_parse))
+
+(clsql:def-view-class hypernyms()
+  ((hyponym :accessor hyponym :type (string 80) :initarg :hyponym)
+   (rank :accessor rank :type integer :initarg :rank)
+   (hypernym :accessor hypernym :type (string 80) :initarg :hypernym))
+  (:base-table hypernyms))
+
+(clsql:def-view-class hyponyms()
+  ((hypernym :accessor hypernym :type (string 80) :initarg :hypernym)
+   (rank :accessor rank :type integer :initarg :rank)
+   (hyponym :accessor hyponym :type (string 80) :initarg :hyponym))
+  (:base-table hyponyms))
+
+(clsql:def-view-class synonyms ()
+  ((orig_word :accessor orig_word :type (string 80) :initarg :orig_word)
+   (synonym :accessor orig_word :type (string 80) :initarg :orig_word)
+   (pos :accessor pos :type integer :initarg :pos)
+   (grammarpos :accessor grammarpos :type (string 15) :initarg  :grammarpos)
+   (definition :accessor definition :type (string 9000) :initarg :definition))
+  (:base-table synonyms))
+
 
 
