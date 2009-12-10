9405fc7d148cd5dc1bab2fcd43c5f0945db7b8e2
David Thole <dthole@tdtdev.thedarktrumpet>
2009-12-09 20:54:32 -0600

    Adding class method for connecting to a database, including the one for SQLite

--- db-connections.lisp
+++ db-connections.lisp
@@ -11,16 +11,33 @@
   (:documentation "The foundation datbase connection class, that the others will inherit"))
 
 (defclass db-connection-sqlite (db-connection)
-  ((type :initform "sqlite" :reader db-type)))
+  ((type :initform "sqlite" :reader db-type))
+  (:documentation "The specific connection object for SQLite")
 
 (defclass db-connection-dbms (db-connection)
   ((host :initarg :host :accessor host-name)
    (user :initarg :user :accessor user-name)
-   (pass :initarg :user :accessor password)
+   (pass :initarg :pass :accessor password)
    (port :initarg :port :accessor port-number))
-  (:documentation "The foundation for databases one connects to through the network"))
+  (:documentation "The foundation for databases one connects to through the network (ODBC, Postgresql, etc)"))
 
 (defclass db-connection-mssql (db-connection-dbms)
-  ((type :initform "odbc" :reader db-type))
-  (:documentation "The specific MSSQL database"))
+  ((type :initform "odbc" :reader db-type)
+   (port :initform NIL :reader port-number))
+  (:documentation "The specific connection object for an MSSQL database"))
 
+
+;;;; Helper connection methods
+
+(defgeneric connect-db (db-connection &optional make-default)
+  (:documentation "Generic method for which the various database types will be supported"))
+
+;; Connection method for MSSQL
+(defmethod connect-db ((db db-connection-mssql) &optional make-default)
+  (clsql:connect (list 
+		  (host-name db)
+		  (user-name db)
+		  (password db))
+		 :database-type :odbc
+		 :make-default make-default))
+  
